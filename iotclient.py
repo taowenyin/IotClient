5d972a7c065d2c5e3a9d1f6913ec72a90213b19b
taowenyin <wenyin.tao@163.com>
2018-01-09 00:52:17 +0800

    添加断网续连功能

--- iotclient.py
+++ iotclient.py
@@ -11,19 +11,51 @@ bridgeClient = BridgeClient()
 
 # Init IOT Server Socket Object
 socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect IOT Server
-socketClient.connect(('10.154.197.144', 7280))
+
+while True:
+    try:
+        # Connect IOT Server
+        socketClient.connect(('39.104.87.214', 7280))
+        socketClient.settimeout(15)
+        # No Error
+        bridgeClient.put('ERROR', '2')
+        break
+    except socket.error, e:
+        print "Can't connect to Server as ", e.errno
+        sleep(1)
+        # Has Error
+        bridgeClient.put('ERROR', '1')
+        continue
 
 while True:
     # Get Server Data
-    data = socketClient.recv(1024)
-    if not data:
-        print('Not data')
-    elif 'LED_ON' == data:
-        print('LED On')
-        # Open LED
-        bridgeClient.put('LED', '1')
-    elif 'LED_OFF' == data:
-        print('LED Off')
-        # Close LED
-        bridgeClient.put('LED', '2')
+    try:
+        data = socketClient.recv(1024)
+        if not data:
+            print('Not data')
+        elif 'LED_ON' == data:
+            print('LED On')
+            # Open LED
+            bridgeClient.put('LED', '1')
+        elif 'LED_OFF' == data:
+            print('LED Off')
+            # Close LED
+            bridgeClient.put('LED', '2')
+    except socket.error, e:
+        print "Can't connect to Server as ", e.errno
+        socketClient.close()
+        socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                # Connect IOT Server
+                socketClient.connect(('39.104.87.214', 7280))
+                socketClient.settimeout(15)
+                # No Error
+                bridgeClient.put('ERROR', '2')
+                break
+            except socket.error, e:
+                print "Can't connect to Server as ", e.errno
+                sleep(1)
+                # Has Error
+                bridgeClient.put('ERROR', '1')
+                continue
